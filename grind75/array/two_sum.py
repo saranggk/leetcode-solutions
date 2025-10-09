0b042b7c4f8bc7b61b3c180147d5197dc6912077
Sarang Kashyap <sarangk@Sarangs-MacBook-Pro.local>
2025-10-08 17:10:28 -0700

    Added Two Sum solution

--- grind75/array/two_sum.py
+++ grind75/array/two_sum.py
@@ -4,3 +4,11 @@ Link: https://leetcode.com/problems/two-sum/
 Difficulty: Easy
 """
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, j in enumerate(nums):
+            diff = target - j
+            if diff in seen:
+                return [seen[diff], i]
+            seen[j] = i
